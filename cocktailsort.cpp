c9f8b85ddfa3dc052be8ce0ff7f59c52c8d98880
Gautham Prabhu M <71874814+GauthamPrabhuM@users.noreply.github.com>
2021-01-20 21:20:23 +0530

    Create cocktailsort.cpp

---
+++ cocktailsort.cpp
@@ -0,0 +1,57 @@
+// C++ implementation of Cocktail Sort
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void CocktailSort(int a[], int n)
+{
+    bool swapped = true;
+    int start = 0;
+    int end = n - 1;
+ 
+    while (swapped) 
+    {
+       
+        swapped = false;
+        for (int i = start; i < end; ++i) 
+        {
+            if (a[i] > a[i + 1]) {
+                swap(a[i], a[i + 1]);
+                swapped = true;
+            }
+        }
+ 
+        if (!swapped)
+            break;
+         swapped = false;
+ 
+        --end;
+         for (int i = end - 1; i >= start; --i) 
+        {
+            if (a[i] > a[i + 1]) {
+                swap(a[i], a[i + 1]);
+                swapped = true;
+            }
+        }
+        ++start;
+    }
+}
+ 
+void printArray(int a[], int n)
+{
+    for (int i = 0; i < n; i++)
+        printf("%d ", a[i]);
+    printf("\n");
+}
+ 
+int main()
+{
+        int a[100];
+        int n;
+        cin>>n;
+        for(int i=0; i<n; i++)
+        cin>>a[i];
+      CocktailSort(a, n);
+    printf("Sorted array :\n");
+    printArray(a, n);
+    return 0;
+}
